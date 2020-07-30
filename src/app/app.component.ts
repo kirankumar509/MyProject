56c303ae97199f501c6d19c7d1fbe81da7a5977b
kirankumar509 <rashwickkiran2015@gmail.com>
2020-07-30 16:53:41 +0530

    MyFiles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import {Employee} from './employee';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'twowaylucase';
-}
+  employee = new Employee('Kiran kumar');	
+	toLower(val:string) {
+	  this.employee.name = val.toLowerCase();
+	}
+}   
