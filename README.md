a02f0d009743d13939ab806fbb1b0f87a5e71ff1
kirankumar509 <68987529+kirankumar509@users.noreply.github.com>
2020-07-30 18:14:33 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Twowaylucase
+# Dropdownonewaybinding
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.4.
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/twowaylucase'),
+      dir: require('path').join(__dirname, './coverage/dropdownonewaybinding'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
